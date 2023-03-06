bab1c81a6b1a3cb80d3f79c3dfacbd22eb6766be
Tim Tully <tully_tim@yahoo.com>
2023-03-06 11:03:52 -0800

    Update Cache.py

--- Cache.py
+++ Cache.py
@@ -7,24 +7,23 @@ from termcolor import colored
 
 class Cache:
     """ OpenAI Cache using Pinecone """
-    __slots__ = 'openaiapi_key', 'pinecone_key', 'pinecone_index', 'epsilon'
+    __slots__ = 'openaiapi_key', 'pinecone_key', 'pinecone_index', 'epsilon', 'model', 'pinecone_index_name'
 
-    MODEL = 'text-embedding-ada-002'
-    INDEX = 'openaicache'
-
-    def __init__(self, openaiapikey, pineconekey, epsilon=0.90):
+    def __init__(self, openaiapikey, pineconekey, pinecone_index_name, epsilon=0.90, model='text-embedding-ada-002'):
         """ Epsilon is essentially cache aggressiveness """
         self.openaiapi_key = openaiapikey
         self.pinecone_key = pineconekey
         openai.api_key = self.openaiapi_key
         pinecone.init(api_key=self.pinecone_key, environment="us-west1-gcp")
-        self.pinecone_index = pinecone.Index(self.INDEX)
+        self.pinecone_index = pinecone.Index(pinecone_index_name)
         self.epsilon = epsilon
+        self.model = model
+        self.pinecone_index_name = pinecone_index_name
 
     def getcompletion(self, prompt):
         res = openai.Completion.create(engine='text-davinci-003', prompt=prompt, temperature=0,
                                        max_tokens=3000, top_p=1, frequency_penalty=0, presence_penalty=0, stop=None)
-        return res['choices'][0]['text'].strip()
+        return res['choices'][0]['text'].strip()        
 
     def __get_from_pinecone(self, embed):
         return self.pinecone_index.query(vector=embed, top_k=3, include_values=False, include_metadata=True)
@@ -36,8 +35,7 @@ class Cache:
     
     def getitem(self, data):
         data = f"Answer the question below.\n\n\nQuestion: {data}\n\nAnswer:"
-        embed = openai.Embedding.create(input=[data], model=self.MODEL)[
-            'data'][0]['embedding']
+        embed = openai.Embedding.create(input=[data], model=self.model)['data'][0]['embedding']
         # cache lookup in PC
         pc_lookup = self.__get_from_pinecone(embed)
         if not pc_lookup.matches:
